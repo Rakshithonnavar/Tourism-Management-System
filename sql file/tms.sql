daab6ed693f4ade556e894f42fb5ce34673c5786
Rakshit honnavar <rakshithonnavar20@gmail.com>
2024-02-23 23:51:14 +0530

    minor chnages

--- sql file/tms.sql
+++ sql file/tms.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 4.9.0.1
--- https://www.phpmyadmin.net/
---
 -- Host: 127.0.0.1
--- Generation Time: Jul 20, 2019 at 10:18 PM
--- Server version: 10.3.15-MariaDB
--- PHP Version: 7.2.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -22,7 +15,7 @@ SET time_zone = "+00:00";
 -- Database: `tms`
 --
 
--- --------------------------------------------------------
+
 
 --
 -- Table structure for table `admin`
@@ -166,9 +159,9 @@ INSERT INTO `tbltourpackages` (`PackageId`, `PackageName`, `PackageType`, `Packa
 =======
 >>>>>>> e041747aff5883d9a00027c98e8a4a43e6820b87
 
--- --------------------------------------------------------
 
---
+
+
 -- Table structure for table `tblusers`
 --
 
